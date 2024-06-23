9e39d2ff2dd0e49a3a49ed0fd74545ded9ecaf4e
Viktoriia <freshitpte@gmail.com>
2024-06-23 17:46:18 +0300

    Add user delete

--- assets/js/createElement.js
+++ assets/js/createElement.js
@@ -18,35 +18,35 @@ function genSingleUser({
   name: { first, last },
   dob: { age },
 }) {
-  // сворити елемент
-  // налаштувати (контент, стилі, атрибути, ...)
-  // навісити обробник
-  // додати елемент в розмітку
-
   const userCard = document.createElement("article");
   rootEl.append(userCard);
   userCard.classList.add("userCard");
 
-  // додати img (створити, задати клас, src з imgSrc,
-  //             alt - конкатенація first, last )
-  const imgEl = document.createElement("img");
-  imgEl.classList.add("userImg");
-  imgEl.src = imgSrc;
-  imgEl.alt = `${first} ${last}`;
-  userCard.appendChild(imgEl);
-
-  const userName = document.createElement("h2");
-  userName.classList.add("userName");
-  userName.textContent = `${first} ${last}`;
-  userCard.append(userName);
-
-  const userAge = document.createElement("p");
-  userAge.classList.add("userAge");
-  userAge.textContent = age;
-  userCard.append(userAge);
+  const imgEl = createNewImg(imgSrc, `${first} ${last}`, "userImg");
+  const userName = createNewElement("h2", `${first} ${last}`, "userName");
+  const userAge = createNewElement("p", age, "userAge");
 
   const trashIcon = document.createElement("button");
   trashIcon.classList.add("trashIcon");
   trashIcon.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-  userCard.append(trashIcon);
+  trashIcon.onclick = function () {
+    this.parentElement.remove();
+  };
+
+  userCard.append(imgEl, userName, userAge, trashIcon);
+}
+
+function createNewImg(src, alt, className) {
+  const newImg = document.createElement("img");
+  newImg.src = src;
+  newImg.alt = alt;
+  newImg.classList.add(className);
+  return newImg;
+}
+
+function createNewElement(type, content, className) {
+  const newElement = document.createElement(type);
+  newElement.textContent = content;
+  newElement.classList.add(className);
+  return newElement;
 }
